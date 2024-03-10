be8a9f0293a14d89bb05a0b79fd7d6d1eeb565a0
Revanth Pothukuchi <revanthpothukuchi123@gmail.com>
2024-03-09 21:27:11 -0500

    feat: added simple TCP server

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use std::net::{SocketAddr, TcpListener, TcpStream};
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+fn get_stream_ip(stream: &TcpStream) -> anyhow::Result<String> {
+    let ip = match stream.local_addr()? {
+        SocketAddr::V4(addr) => addr.ip().to_string(),
+        SocketAddr::V6(addr) => addr.ip().to_string(),
+    };
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    Ok(ip)
+}
+
+fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    
+    println!("[redis] server started");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let ip = get_stream_ip(&stream)?;
+                println!("[redis] connection established with {}", ip);
+            }
+            Err(err) => {
+                eprintln!("[redis - error] unknown error occurred: {}", err);
+            }
+        }
+    }
+
+    Ok(())
 }
